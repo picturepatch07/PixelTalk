f812ce126df45bf1044084f31231771f783a512f
Abhishek <anand2000ys@gmail.com>
2023-08-06 11:36:45 +0530

    lobbyScreen modified

--- client/src/pages/LobbyScreen.jsx
+++ client/src/pages/LobbyScreen.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
 const LobbyScreen = () => {
+  const [email, setEmail] = useState("");
+  const [room, setRoom] = useState("");
+
+  const handleSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log({ email, room });
+    },
+    [email, room]
+  );
+
   return (
     <div>
       <h1>Lobby</h1>
-      <form>
+      <form onSubmit={handleSubmitForm}>
         <label htmlFor="email">Email ID</label>
-        <input type="email" name="email" id="email" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <br />
         <label htmlFor="room">Room Number</label>
-        <input type="text" name="room" id="room" />
+        <input
+          type="text"
+          name="room"
+          id="room"
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+        />
         <br />
         <button>JOIN</button>
       </form>
