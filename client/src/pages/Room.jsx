9a1ee25023ddc4e7adb337f96d47716eea6326b2
Abhishek <anand2000ys@gmail.com>
2023-08-13 13:01:49 +0530

    video and audio on current screen

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -1,13 +1,23 @@
 import React, { useEffect, useCallback, useState } from "react";
+import ReactPlayer from "react-player";
 import { useSocket } from "../context/SocketProvider";
 
 const RoomPage = () => {
   const socket = useSocket();
   const [remoteSocketId, setRemoteSocketId] = useState(null);
+  const [myStream, setMyStream] = useState(null);
 
   const handleUserJoined = useCallback(({ email, id }) => {
     console.log(`Email ${email} joined room`);
     setRemoteSocketId(id);
+  }, []);
+
+  const handleCallUSer = useCallback(async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    setMyStream(stream);
   }, []);
 
   useEffect(() => {
@@ -21,6 +31,10 @@ const RoomPage = () => {
     <div>
       <h1>Room Page</h1>
       <h4>{remoteSocketId ? "Connected" : "No one in room"}</h4>
+      {remoteSocketId && <button onClick={handleCallUSer}>CALL</button>}
+      {myStream && (
+        <ReactPlayer height="200px" width="200px" url={myStream} playing />
+      )}
     </div>
   );
 };
